ace3aa8913aed5310e83c467a68c97d2443ebb0f
clipklop <siliconchik@gmail.com>
2021-05-09 01:00:41 +0300

    Make preparation for Heroku deployment

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +137,7 @@ STATIC_URL = '/static/'
 # Django runs the code in topics() only if they are. If the user isn’t logged in,
 # they’re redirected to the login page
 LOGIN_URL = 'users:login'
+
+# Heroku settings.
+import django_heroku
+django_heroku.settings(locals())
